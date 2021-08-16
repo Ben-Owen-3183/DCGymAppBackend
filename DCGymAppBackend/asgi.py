72acbab4b5954eebb1bbf198428eea3f9ef56be8
Ben <ben.owen3183@gmail.com>
2021-08-16 18:05:51 +0100

    moved setup

--- DCGymAppBackend/asgi.py
+++ DCGymAppBackend/asgi.py
@@ -7,15 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-django.setup()
 import os
+import django
+django.setup()
 
 # from django.core.asgi import get_asgi_application
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.http import AsgiHandler
-import django
+
 import messenger.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DCGymAppBackend.settings')
