921ed12c652c29dcc9389da58d1b20227390c1df
Ben <ben.owen3183@gmail.com>
2021-09-21 16:32:42 +0100

    added fetch user data view

--- login/urls.py
+++ login/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path('login', views.login, name='login'),
     # path('signup', views.signup, name='signup'),
-    path('login/', views.login.as_view())
+    path('login/', views.login.as_view()),
+    path('user_data/', views.userData.as_view())
 ]
--- login/views.py
+++ login/views.py
@@ -4,6 +4,39 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from user_account.models import UserAvatar
 from .membership_status import member_status_checker
+from rest_framework.views import APIView
+from login.models import CustomUser
+import logging
+
+class userData(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            token = request.data['token']
+            user_id = Token.objects.get(key=token).user_id
+            user = CustomUser.objects.get(id=user_id)
+
+            avatarName = None
+            try:
+                avatar = UserAvatar.objects.get(user=user.pk)
+                avatarName = avatar.image_name
+            except:
+                avatarName = ''
+
+            return Response({'user_data' : {
+                'token': token,
+                'user_id': user.pk,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'is_staff': user.is_staff,
+                'is_superuser': user.is_superuser,
+                'avatarURL': avatarName,
+            }})
+        except Exception as e:
+            logging.exception('Get Chat')
+            return Response({'error': 'failed request'})
 
 
 class login(ObtainAuthToken):
