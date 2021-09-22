c406b0a54539f27ca2dac35cf54ca2c5616119ee
Ben <ben.owen3183@gmail.com>
2021-09-22 18:08:49 +0100

    edit for static to main url file

--- DCGymAppBackend/urls.py
+++ DCGymAppBackend/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('timetable/', include('timetable.urls')),
     path('video/', include('live_stream.urls')),
     path('policies/', include('policies.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
